53fcc7125189ba1009f93f884ed837a642e95eb1
Tee Noel <noeltumbo88@gmail.com>
2025-08-07 10:30:35 +0300

    Data structures assignment

---
+++ Task_2.py
@@ -0,0 +1,26 @@
+# Create an empty list
+my_list = []
+
+# Append elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+# Extend my_list with another list
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of value 30
+print(my_list.index(30))
+
+# Print the final list to verify
+print(my_list)
